eb1481423af86c392f1451a6acc65c39c05130a7
marc sanchez <adaw23msanchez@inslaferreria.cat>
2024-02-26 20:55:34 +0100

    afegit hello world!! doble

---
+++ projecteProva2024/src/projecteprova2024/Prova.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteprova2024;
+
+/**
+ *
+ * @author itmar
+ */
+public class Prova {
+    
+}
